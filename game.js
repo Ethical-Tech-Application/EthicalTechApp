b7e1931754393586bd62f283552883ebb8c5eb11
Ishani Sadagopan Jeyaram Malismitha <ishanisadagopan04jm@gmail.com>
2024-07-23 13:28:07 -0500

    Scenario 1 Completed

--- game.js
+++ game.js
@@ -7,7 +7,12 @@ let input = document.getElementById('input');
 //this is the variable for the name of the character
 let character;
 //score
-let totPts;
+let totPts = 0;
+
+let addPt = function(pt = 0)
+{
+  totPts += pt;
+};
 
 //this is how after we type in the character name and hit enter
 //we will add the name to the variable, remove the input box and start our first scenario
@@ -17,8 +22,8 @@ input.onkeypress = function(event)
   if(event.key == "Enter" || event.keyCode == 13)
   {
     character =  input.value;
-    input.parentNode.removeChild(input)
-    advanceTo(scenario.two)
+    input.parentNode.removeChild(input);
+    advanceTo(scenario.zero);
   }
 };
 
@@ -40,113 +45,111 @@ let changeButtons = function(buttonList)
   buttonBox.innerHTML = "";
   for(let i = 0; i < buttonList.length; i++) 
   {
-    buttonBox.innerHTML += "<button onClick="+buttonList[i][1]+">" + buttonList[i][0] + "</button>";
+    buttonBox.innerHTML += "<button onClick=" + buttonList[i][1] + ">" + buttonList[i][0] + "</button>";
   };
 };
 
 //this is what moves the game along
-let advanceTo = function(s)
+let advanceTo = function(scenario)
 {
-  changeImage(s.image)
-  changeText(s.text)
-  changeButtons(s.buttons)
+  changeImage(scenario.image);
+  changeText(scenario.text);
+  changeButtons(scenario.buttons);
+  addPt(scenario.ptsAdded);
+  //alert(totPts);
 };
 
 //this is the object that holds each scenario, the more you add the more options there are
 //scenario = {}
 let scenario =
 {
-  one:
+  start:
   {
     image: "./img/ETG.png", //Ethical Tech image
     text: "This is an ethical technology text-based game with interactive experience where players are presented with scenarios and dilemmas related to the ethical considerations and implications of technology development and use. The game involves making choices that impact various stakeholders, such as users, society, and the environment, and encourages players to reflect on the consequences of their decisions.<br><br> What is your name?\n",
   },
-  two:
+  zero:
   {
     image: "./img/AInew.jpg", //image of TechGuard Company
     text: "Your character is a bright young software developer who recently joined Ethical Tech Guard, a leading technology company known for its cutting-edge innovations. As you navigate your career, you are faced with numerous ethical dilemmas that will test your values and shape the future of technology.<br><br> What do you want to do?",
-    buttons: [["Proceed to Scenario", "advanceTo(scenario.three)"],["Character Interaction", "advanceTo(scenario.twelve)"]]
+    buttons: [["Proceed to Scenario", "advanceTo(scenario.sen1)"],["Character Interaction", ""]],
   },
-  three:
+
+  sen1:
   {
     image: "./img/socialMedia.jpg", //image of TechGuard Team
-    text: "Your team is developing a new social media app that requires collecting user data to improve personalization.<br> You are faced with 2 choices:<br><br> Choice A: Implement robust data privacy measures, ensuring users have control over their data and transparency about its use.<br><br> Choice B: Collect extensive data without explicit user consent to accelerate development and gain competitive advantage. ",
-    buttons: [["Choice A", "advanceTo(scenario.four)"],["Choice B", "advanceTo(scenario.six)"]]
+    text: "Your team is developing a new social media app that requires collecting user data to improve personalization.<br><br> Choice A: Implement robust data privacy measures, ensuring users have control over their data and transparency about its use.<br><br> Choice B: Collect extensive data without explicit user consent to accelerate development and gain competitive advantage. ",
+    buttons: [["Choice A", "advanceTo(scenario.A11)"],["Choice B", "advanceTo(scenario.B11)"]]
   },
-  four:
-  {
-    image: "#",//image of consequence
-    text: "Consequence: Slower development process, but users trust and appreciate the app, leading to long-term success. And now you are faced with more decisions: <br><br> As you implement these measures, you encounter a bug that delays the project further. Do you: <br><br> Option A: Inform your team and work overtime to fix the bug. <br><br> Option B: Ignore the bug for now, planning to fix it post-launch.",
-    buttons: [["Option A", "advanceTo(scenario.eight)"],["Option B", "advanceTo(scenario.five)"]]
-  },
-  five:
-  {
-    image: "#",
-    text: "User feedback suggests that the privacy measures are too cumbersome, impacting user experience.Do you: <br><br> Option 1: Simplify the measures while maintaining core privacy features. <br><br> Option 2:  Stick with the current plan, prioritizing privacy over convenience. ",
-    buttons: [["Option 1", "advanceTo(scenario.eight)"],["Option 2", "advanceTo(scenario.nine)"]]
-  },
-  six:
-  {
-    image: "#",//image of consequence (lack of transparency)
-    text: "Consequence: Faster development and short-term success, but potential backlash and legal issues as users discover the lack of transparency. <br>A whistleblower within the company threatens to go public with the unethical data practices. Do you: <br><br> Option A: Attempt to silence the whistleblower through a legal agreement. <br><br> Option B: Address the issue openly and commit to changing your data practices. ",
-    buttons: [["Option A", "advanceTo(scenario.seven)"],["Option B", "advanceTo(scenario.seven)"]]
-  },
-  seven:
-  {
-    image: "#",
-    text: "An influential tech journalist discovers your data practices and contacts you for a statement. Do you: <br><br> Option 1: Provide a transparent response and outline your plans for change.<br><br> Option 2: Deflect the questions and try to control the narrative. ",
-    buttons: [["Option 1", "advanceTo(scenario.eight)"],["Option 2", "advanceTo(scenario.nine)"]]
-  },
-  eight:
-  {
-    image: "#",
-    text: "Users appreciate the high level of privacy protection. The app gains a reputation for respecting user data, leading to increased trust and user retention.",
-    buttons: [["continue", "advanceTo(scenario.ten)"]]
-  },
-  nine:
-  {
-    image: "#",
-    text: "Users discover hidden data collection practices, resulting in outrage and legal challenges. The app faces lawsuits and regulatory scrutiny.",
-    buttons: [["continue", "advanceTo(scenario.fifteen)"]]
-  },
-  ten:
-  {
-    image: "#",
-    text: "Ethical Tech Guard is developing an algorithm to increase user engagement on a new social media platform. You discover it is promoting divisive and inflammatory content. <br><br> Choice A: Redesign the algorithm to prioritize balanced and factual content.<br><br> Choice B: Maintain the current algorithm to maximize engagement. ",
-    buttons: [["Choice A", "advanceTo(scenario.eleven)"],["Choice B", "advanceTo(scenario.thirteen)"]]
-  },
-  eleven:
-  {
-    image: "#",
-    text: "Consequence: Reduced engagement metrics, but a healthier online environment. Which path will you take? <br><br> Path A: Users initially react negatively to the less sensational content.<br>Educate users about the benefits of balanced content.<br><br>Path B: Advertisers pull back due to lower engagement.<br> Seek new advertisers who value ethical practices.  ",
-    buttons: [["Path A", "advanceTo(scenario.eleven)"],["Path B", "advanceTo(scenario.eleven)"]]
-  },
-  twelve:
-  {
-    image: "#",
-    text: "TO BE CONTINUED...",
-    buttons: [["continue", "advanceTo(scenario.two)"]]
-  },
-  thirteen:
-  {
-    image: "#",
-    text: "TO BE CONTINUED...",
-    buttons: [["continue", "advanceTo(scenario.two)"]]
-  },
-  fourteen:
-  {
-    image: "#",
-    text: "TO BE CONTINUED...",
-    buttons: [["continue", "advanceTo(scenario.two)"]]
-  },
+    A11: // A
+    {
+      ptsAdded: 1,
+      image: "#",
+      text: "As you implement these measures, you encounter a bug that delays the project further.<br><br> Path 1: Inform your team and work overtime to fix the bug.<br><br> Path 2:  Ignore the bug for now, planning to fix it post-launch.",
+      buttons: [["Path 1", "advanceTo(scenario.A111)"],["Path 2", "advanceTo(scenario.A12)"]]
+    },
+      A111: // A.1 Correct
+      {
+        ptsAdded: 1,
+        image: "#",
+        text: "+",
+        buttons: [["Proceed", "advanceTo(scenario.A12)"]]
+      },
+    A12:
+    {
+      image: "#",
+      text: "User feedback suggests that the privacy measures are too cumbersome, impacting user experience.<br><br> Path 1: Simplify the measures while maintaining core privacy features.<br><br> Path 2: Stick with the current plan, prioritizing privacy over convenience.",
+      buttons: [["Path 1", "advanceTo(scenario.sen2)"],["Path 2", "advanceTo(scenario.A112)"]]
+    },
+      A122: // A.2 Correct
+      {
+        ptsAdded: 1,
+        image: "#",
+        text: "+",
+        buttons: [["Proceed", "advanceTo(scenario.sen2)"]]
+      },
+    B11: // B
+    {
+      image: "#",
+      text: "As you implement these measures, you encounter a bug that delays the project further.<br><br> Path 1: Inform your team and work overtime to fix the bug.<br><br> Path 2:  Ignore the bug for now, planning to fix it post-launch.",
+      buttons: [["Path 1", "advanceTo(scenario.B12)"],["Path 2", "advanceTo(scenario.B112)"]]
+    },
+      B112: // B.1 Correct
+      {
+        ptsAdded: 1,
+        image: "#",
+        text: "+",
+        buttons: [["Proceed", "advanceTo(scenario.A12)"]]
+      },
+    B12:
+    {
+      image: "#",
+      text: "User feedback suggests that the privacy measures are too cumbersome, impacting user experience.<br><br> Path 1: Simplify the measures while maintaining core privacy features.<br><br> Path 2: Stick with the current plan, prioritizing privacy over convenience.",
+      buttons: [["Path 1", "advanceTo(scenario.121)"],["Path 2", "advanceTo(scenario.sen2)"]]
+    },
+      B121: // B.2 Correct
+      {
+        ptsAdded: 1,
+        image: "#",
+        text: "+",
+        buttons: [["Proceed", "advanceTo(scenario.sen2)"]]
+      },
+
+
+
+
+
+
+
+
+
   fifteen:
   {
     image: "#",
     text: "Reflections and Learnings:<br><br> Ethical Use of Data: <br> Reflection: Consider the ethical implications of collecting extensive personal data from users without explicit consent or understanding. <br> Learning: Understand the importance of transparency and informed consent in data collection practices. Discuss how companies should balance their business interests with user privacy rights. <br><br> Impact on Society: <br> Reflection: Consider the broader societal impacts of extensive data collection and targeted advertising, including implications for democracy, polarization, and social cohesion.  <br><br> Learning: Discuss the role of social media platforms in shaping public discourse and the ethical considerations of influencing user behavior through targeted content and ads.",
-    buttons: [["continue", "advanceTo(scenario.two)"]]
+    buttons: [["continue", "advanceTo(scenario.zero)"]]
   }
 };
 
-
 //this is the code that starts the game
-advanceTo(scenario.one);
+advanceTo(scenario.start);
